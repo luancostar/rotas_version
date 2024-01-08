27e429c0ee43b650f472d9fa64ad14be1bfca961
Luan Rodrigues <luancostar@gmail.com>
2024-01-08 18:02:37 -0300

    update first commit

--- components/header.php
+++ components/header.php
@@ -292,5 +292,47 @@
         </a>
     </div>
   <!-- end dash troggle-icon -->
+ <script>
+    document.addEventListener('DOMContentLoaded', function () {
+  const darkModeCheckbox = document.getElementById('dark');
+  const lightModeCheckbox = document.getElementById('light');
+  const themeStyle = document.getElementById('theme-style');
 
+  // Verifica o valor armazenado no localStorage
+  const savedTheme = localStorage.getItem('theme');
+
+  // Aplica o tema salvo ou o tema padrão
+  if (savedTheme) {
+    themeStyle.href = `${savedTheme}.css`;
+    if (savedTheme === 'dark') {
+      darkModeCheckbox.checked = true;
+    } else {
+      lightModeCheckbox.checked = true;
+    }
+  }
+
+  // Adiciona listeners para os checkboxes
+  darkModeCheckbox.addEventListener('change', function () {
+    if (darkModeCheckbox.checked) {
+      themeStyle.href = 'dark.css';
+      localStorage.setItem('theme', 'dark');
+      lightModeCheckbox.checked = false;
+    } else {
+      themeStyle.href = '';
+      localStorage.removeItem('theme');
+    }
+  });
+
+  lightModeCheckbox.addEventListener('change', function () {
+    if (lightModeCheckbox.checked) {
+      themeStyle.href = 'light.css';
+      localStorage.setItem('theme', 'light');
+      darkModeCheckbox.checked = false;
+    } else {
+      themeStyle.href = '';
+      localStorage.removeItem('theme');
+    }
+  });
+});
+ </script>
 </header>
---
+++ components/scripts.php
@@ -0,0 +1,19 @@
+<script src="'../'assets/js/dark0light.js"></script>
+<script src="../assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/libs/metismenujs/metismenujs.min.js"></script>
+<script src="../assets/libs/simplebar/simplebar.min.js"></script>
+<script src="../assets/libs/feather-icons/feather.min.js"></script>
+
+<!-- apexcharts -->
+<script src="../assets/libs/apexcharts/apexcharts.min.js"></script>
+
+<!-- Vector map-->
+<script src="../assets/libs/jsvectormap/js/jsvectormap.min.js"></script>
+<script src="../assets/libs/jsvectormap/maps/world-merc.js"></script>
+
+<!-- swiper js -->
+<script src="../assets/libs/swiper/swiper-bundle.min.js"></script>
+
+<script src="../assets/js/pages/dashboard.init.js"></script>
+
+<script src="../assets/js/app.js"></script>
